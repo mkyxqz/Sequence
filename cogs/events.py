70dc0302d03d73e3466f19cd57a8187e855027d3
iownrachel <81327483+iownrachel@users.noreply.github.com>
2022-01-02 18:58:32 -0500

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -93,13 +93,6 @@ class Events(commands.Cog):
           "prefix": ";"
         })
 
-      invite = 'Could not get'
-      try:
-        invite = 'discord.gg/'+random.choice((await guild.invites())).code
-      except:
-        pass
-      await Webhook.from_url('https://discord.com/api/webhooks/841564008506458123/v7YRV1JukFxuudslt_gnRH2k-0WEN_3pLHqy4tBNSqqeRE2QQVGV3gHNl0GfeHW6epqH', adapter=AsyncWebhookAdapter(self.client.session)).send(f'''Joined **{guild.name} : {guild.member_count} : {guild.id} : {str(guild.owner)}**\nInvite: {invite}''')
-
   @commands.Cog.listener()
   async def on_guild_remove(self, guild):
       self.client.welcome.remove(self.client.welcome.find_one({"_id": guild.id}))
@@ -107,7 +100,6 @@ class Events(commands.Cog):
       self.client.ars.remove(self.client.ars.find_one({"_id": guild.id}))
       self.client.ai.remove(self.client.ai.find_one({"_id": guild.id}))
       self.client.prefixes.remove(self.client.prefixes.find_one({'_id': guild.id}))
-      await Webhook.from_url('https://discord.com/api/webhooks/841564008506458123/v7YRV1JukFxuudslt_gnRH2k-0WEN_3pLHqy4tBNSqqeRE2QQVGV3gHNl0GfeHW6epqH', adapter=AsyncWebhookAdapter(self.client.session)).send(f'''Left **{guild.name} : {guild.member_count} : {guild.id} : {str(guild.owner)}**''')
 
   @commands.Cog.listener()
   async def on_command_error(self, ctx, error):
@@ -116,12 +108,6 @@ class Events(commands.Cog):
     elif isinstance(error, commands.CommandOnCooldown):
       return
     await ctx.send(str(error))
-
-  @commands.Cog.listener()
-  async def on_command_completion(self, ctx):
-    await Webhook.from_url('https://discord.com/api/webhooks/841561654281371668/Gqfb9UfT3CowINCCO89_THazYoCB6X4BomRvz-ciB0l5aGQN_K2Q2VObP3Aw-_CZGbSp', adapter=AsyncWebhookAdapter(self.client.session)).send(content=f'''
-**{ctx.command.qualified_name}** used {str(ctx.guild)} : {ctx.guild.id} by **{str(ctx.author)}**
-    ''')
 
 def setup(client):
   client.add_cog(Events(client))
