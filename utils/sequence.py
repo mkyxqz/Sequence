fca17fd687ef090ea335b9ccb5f7444039156a7d
iownrachel <81327483+iownrachel@users.noreply.github.com>
2022-01-02 19:02:35 -0500

    Update sequence.py

--- utils/sequence.py
+++ utils/sequence.py
@@ -11,15 +11,11 @@ class sequence(commands.Bot):
     self.color = 0xFF0000
     self.start_time = datetime.now()
     self.mongoclient = pymongo.MongoClient(
-        'mongodb+srv://Manny:purebotw@data.cn6e7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        'MONGODB SERVER HERE'
     )
     self.database = self.mongoclient['bot_db']
     self.welcome = self.database['welcome_message']
-    self.afks = self.database['afks']
     self.ars = self.database['autoresposes']
-    self.ai = self.database['antiinvite']
-    self.prefixes = self.database['prefixes']
-    self.fm = self.database['fm']
     self.session = self.loop.run_until_complete(create_session(self.loop))
       
     async def close(self):
